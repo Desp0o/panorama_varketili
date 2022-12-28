34f94f82c127749789d2bc77a6c16b7157638d89
Tornike Despotashvili <95018466+Desp0o@users.noreply.github.com>
2022-12-28 00:50:19 -0800

    website update vol.III

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -1,4 +1,5 @@
 import {React, useEffect, useState, useRef} from "react";
+import emailjs from '@emailjs/browser';
 import {Link} from  "react-router-dom";
 import logo from '../../images/logo.svg'
 import '../../index.css'
@@ -7,7 +8,62 @@ import'./AboutProject.css'
 import line29 from '../../images/Line29.png'
 import sofa from '../../images/sofa.png'
 
+import '../../styles/PopUpMsg.css'
+
+import callFloatBtn from '../../images/callFloatBtn.png'
+import xMark from '../../images/close.png'
+
 export default function AboutProject(){
+
+    //Floating Button Appear
+    const scrollRef = useRef(null)
+    const floatButtonRef = useRef()
+    const [floatBtnClass, setFloatBtnClass] = useState('call_float_btn')
+    const [addHeight, setAddHeight] = useState(200) 
+
+    function floatBtnHandler() {
+        if(scrollRef.current.getBoundingClientRect().width > 1300){
+            setAddHeight(0)
+        }
+
+        if(window.scrollY > scrollRef.current.getBoundingClientRect().height/2+addHeight){
+            setFloatBtnClass('call_float_btn call_float_btn_active')
+            
+        }else{
+            setFloatBtnClass('call_float_btn')
+        }
+    }
+
+    useEffect(()=>{
+        
+        window.addEventListener('scroll',floatBtnHandler)
+
+        return () => {
+            window.removeEventListener('scroll',floatBtnHandler);
+          };
+    },[window.scrollY])
+    //End Floating Button Appear
+
+    // Email send Functionality
+    const form = useRef();
+    const inputNameRef = useRef()
+    const inputNumberRef = useRef()
+    const inputMailRef = useRef()
+    const inputSendref = useRef()
+    const [popUp, setPopUp] = useState('pop_up_msg')
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs.sendForm('service_bpng7e7', 'template_y2xffsh', form.current, 'k5sgg72-uloGuXB_E')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+          e.target.reset()
+    };
+    // End Of Email Functionality
 
     const GeoLanguage = useRef()
     const navbarLinkFlats = useRef()
@@ -130,13 +186,51 @@ export default function AboutProject(){
                 </div>
             </div>
 
+            <div className={popUp}>
+            <div className="overlay" onClick={()=>setPopUp('pop_up_msg')}></div>
+
+            <div className="pop_up_msg_inner">
+
+                <img src={xMark} className="xMark" alt="xmarkBtn" onClick={()=>setPopUp('pop_up_msg')}/>
+                
+                <h1 className="pop_up_header">დაგვიტოვე საკონტაქტო</h1>
+
+                <form ref={form} onSubmit={sendEmail}>
+                    <div className="pop_up_msg_inner_top">
+                        <input ref={inputNameRef} type="text" name="user_name" placeholder="სახელი" important="true"/>
+                        <input ref={inputNumberRef} type="text" name="user_number" placeholder="ნომერი" important="true"/>
+                    </div>
+
+                    <div className="pop_up_msg_inner_middle">
+                        <input ref={inputMailRef} className="email" type="email" name="user_email"  placeholder="ელ.ფოსტა" important="true"/>
+                    </div>
+
+                    <div className="pop_up_msg_inner_bottom">
+                        <div className="pop_up_msg_inner_bottom submit_btn">
+                            <button type="submit">
+                                <span ref={inputSendref}>გაგზავნა</span>
+                            </button>
+                        </div>
+                    </div>
+                </form>
+
+            </div>
+            </div>
+
+            <img 
+            src={callFloatBtn} 
+            ref={floatButtonRef} 
+            className={floatBtnClass} 
+            onClick={()=> setPopUp('pop_up_msg pop_up_active')}
+            />
+
             <div className={fadeOut}>
                 <h1 className="about_proj_page_header">ლოკაცია</h1>
                 <p className="about_proj_page_p">ვარკეთილის პროექტი მდებარეობს ვიქტორ კუპრაძის 68<br/> ნომერში, ჰუალინგ თბილისი პლაზას ახალ ქალაქში,<br/> ცენტრალური გზის პირას.</p>
                 <p className="about_proj_page_p"> ჩვენი პროექტის ყველა ბინას აქვს საუკეთესო ხედები.</p>
             </div>
 
-            <div className={map}>
+            <div className={map} ref={scrollRef}>
                 <iframe className="map_frame" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d28337.617671515094!2d44.87096419968619!3d41.70702743657408!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4044127860f0ad61%3A0x8f893e58130af269!2zNjgg4YOV4YOY4YOl4YOi4YOd4YOgIOGDmeGDo-GDnuGDoOGDkOGDq-GDmOGDoSDhg6Xhg6Phg6nhg5AsIOGDl-GDkeGDmOGDmuGDmOGDoeGDmA!5e0!3m2!1ska!2sge!4v1672070202149!5m2!1ska!2sge" referrerPolicy="no-referrer-when-downgrade"></iframe>
             </div>
 
