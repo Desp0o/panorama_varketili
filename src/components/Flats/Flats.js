65abf6a6a48ece6a5c3293b3cd9c46536a5e1165
Tornike Despotashvili <95018466+Desp0o@users.noreply.github.com>
2023-01-09 01:50:38 -0800

    implemented swiper js

--- src/components/Flats/Flats.js
+++ src/components/Flats/Flats.js
@@ -1,6 +1,8 @@
 import {React, useEffect, useState, useRef} from "react";
 import {Link} from  "react-router-dom";
 
+
+
 import renderB02 from '../../images/RenderB02.png'
 import renderB04 from '../../images/RenderB04.png'
 import renderB03 from '../../images/RenderB03.png'
@@ -8,6 +10,12 @@ import meterCoub from '../../images/meterCoube.png'
 import bed from '../../images/bed.png'
 import leftArrow from '../../images/leftArrow.png'
 import rightArrow from '../../images/rightArrow.png'
+
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import {Swiper, SwiperSlide} from "swiper/react";
+import { Navigation } from "swiper";
 
 
 import './Flats.css'
@@ -33,7 +41,7 @@ const renderArr =[
         id: 3,
         image: `${renderB03}`,
         text1: '/ 76.6 M2',
-        text2: '/ 2',
+        text2: '/ 1',
         link: '/components/Apartments/apartmentsInner/Apartment76'
     }
 ]
@@ -59,11 +67,7 @@ export default function Flats(props){
 
     },[props.iseng, props.isrus, props.isgeo])
 
-    const cardsListRef = useRef()
-    let box = document.querySelector('.cards')
-    function toLeft() {
-        box.scrollLeft = box.scrollLeft+300;
-    }
+   
     
 
     return(
@@ -78,7 +82,7 @@ export default function Flats(props){
                     renderArr.map((render)=>{
                         return(
                             
-                            <>
+                            <div className="maping_cards">
                                 <div key={render.id}  className="card" >
                                 <Link to={render.link}>
                                     <div className="card_inner_top">
@@ -98,11 +102,117 @@ export default function Flats(props){
                                     </div>
                                     </Link>
                                 </div>
-                            </>
+                            </div>
                             
                         )
                     })
                 }
+
+
+                {/* swiper slider */}
+                <div className="swiper_slider_responsive">
+                    <Swiper
+                        slidesPerView={'auto'}
+                        spaceBetween={15}
+                        centeredSlides={true}
+                        modules={[Navigation]}
+                        className="swiper"
+                        navigation={{
+                            
+                            prevEl: '.leftBtn',
+                            nextEl: '.rightBtn'
+                        }}
+                    >
+                        <SwiperSlide>
+                            <div className="card_outer_div">
+                                <div  className="card" >
+                                    <Link to='/components/Apartments/apartmentsInner/Apartment90'>
+                                        <div className="card_inner_top">
+                                            <img className="card_render_main_screen" src={renderB02} />
+                                        </div>
+
+                                        <div className="card_inner_bottom">
+                                            <div className="card_inner_bottom_left">
+                                                <img className="mc_Icon" src={meterCoub} />
+                                                <p className="card_inner_bottom_p">/ 90 M2</p>
+                                            </div>
+
+                                            <div className="card_inner_bottom_right">
+                                                <img className="bed_Icon" src={bed} />
+                                                <p className="card_inner_bottom_p">/ 2</p>
+                                            </div>
+                                        </div>
+                                    </Link>
+                                </div>
+
+                                <div className="leftBtn"><img src={leftArrow} alt='swiper buttons'/></div>
+                                <div className="rightBtn"><img src={rightArrow} alt='swiper buttons'/></div>
+
+                            </div>
+                        </SwiperSlide>
+
+                        <SwiperSlide>
+                            <div className="card_outer_div">
+                                <div  className="card" >
+                                    <Link to='/components/Apartments/apartmentsInner/Apartment71Sec'>
+                                        <div className="card_inner_top">
+                                            <img className="card_render_main_screen" src={renderB04} />
+                                        </div>
+
+                                        <div className="card_inner_bottom">
+                                            <div className="card_inner_bottom_left">
+                                                <img className="mc_Icon" src={meterCoub} />
+                                                <p className="card_inner_bottom_p">/ 71.5 M2</p>
+                                            </div>
+
+                                            <div className="card_inner_bottom_right">
+                                                <img className="bed_Icon" src={bed} />
+                                                <p className="card_inner_bottom_p">/ 2</p>
+                                            </div>
+                                        </div>
+                                    </Link>
+                                </div>
+
+                                <div className="leftBtn"><img src={leftArrow} alt='swiper buttons'/></div>
+                                <div className="rightBtn"><img src={rightArrow} alt='swiper buttons'/></div>
+
+                            </div>
+                        </SwiperSlide>
+                        
+                        <SwiperSlide>
+                            <div className="card_outer_div">
+                                <div  className="card" >
+                                    <Link to='/components/Apartments/apartmentsInner/Apartment76'>
+                                        <div className="card_inner_top">
+                                            <img className="card_render_main_screen" src={renderB03} />
+                                        </div>
+
+                                        <div className="card_inner_bottom">
+                                            <div className="card_inner_bottom_left">
+                                                <img className="mc_Icon" src={meterCoub} />
+                                                <p className="card_inner_bottom_p">/ 76.6 M2</p>
+                                            </div>
+
+                                            <div className="card_inner_bottom_right">
+                                                <img className="bed_Icon" src={bed} />
+                                                <p className="card_inner_bottom_p">/ 1</p>
+                                            </div>
+                                        </div>
+                                    </Link>
+                                </div>
+
+                                <div className="leftBtn"><img src={leftArrow} alt='swiper buttons'/></div>
+                                <div className="rightBtn"><img src={rightArrow} alt='swiper buttons'/></div>
+
+                            </div>
+                        </SwiperSlide>
+                        
+                    </Swiper>
+                    <div className='btns'></div>
+                    <div className='next'></div>
+                </div>
+                {/* ************** */}
+
             </div>
 
             <div className="btnOuter">
